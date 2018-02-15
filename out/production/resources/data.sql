828c1fa1b27833cf9d77ce7368de1b253b41708b
NastyaNasalevich <nastya.nasalevich@gmail.com>
2018-02-15 18:19:27 +0300

    updates done by...

---
+++ out/production/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO user VALUES (1, 'Nastya', '12345678', 'ROLE_ADMIN');
--- out/production/resources/schema.sql
+++ out/production/resources/schema.sql
@@ -1,9 +1,9 @@
+DROP TABLE user IF EXISTS;
+
 CREATE TABLE user (
   id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  role ENUM('ROLE_ADMIN', 'ROLE_USER') NOT NULL
+  role     VARCHAR(255),
+  CHECK (role='ROLE_ADMIN' OR role='ROLE_USER')
 );
-
-INSERT INTO user VALUES (1, 'Nastya', '12345678', 'ROLE_ADMIN');
-
